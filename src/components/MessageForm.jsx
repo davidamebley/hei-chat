ac579783b334b8667387dad3520f8d44a26eaafb
David Amebley <davameb@gmail.com>
2023-12-27 06:13:47 +0200

    Add MessageForm component with state and event handlers

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 const MessageForm = () => {
+    const [value, setValue] = useState('');
+
+    const handleSubmit = () => { }
+    const handleChange = () => { }
+
     return (
-        <div>
-            Message Form
-        </div>
+        <form className="message-form" onSubmit={handleSubmit}>
+            <input
+                className="message-input"
+                placeholder="Create a message..."
+                value={value}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+            />
+        </form>
     );
 }
 
