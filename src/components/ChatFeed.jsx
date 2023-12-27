94b8b8c2f4d710c15a47769e3f122ea89cbe319a
David Amebley <davameb@gmail.com>
2023-12-27 04:35:57 +0200

    Update ChatFeed component to conditionally render OwnMessage or OthersMessage based on isMyMessage

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,7 +1,53 @@
-const ChatFeed = (props) => {
-    return (
-        <div>
+import MessageForm from "./MessageForm";
+import OwnMessage from "./OwnMessage";
+import OthersMessage from "./OthersMessage";
 
+const ChatFeed = (props) => {
+    const { chats, activeChat, userName, messages } = props;
+
+    const chat = chats && chats[activeChat];
+
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+
+        return keys.map((key, index) => {
+            const message = messages[key];
+            const lastMessageKey = index === 0 ? null : messages[keys[index - 1]];
+            const isMyMessage = userName === message.sender.userName;
+
+            return (
+                <div key={`msg_${index}`} style={{ width: '100%' }}>
+                    <div className="message-block">
+                        {
+                            isMyMessage
+                                ? <OwnMessage message={message} />
+                                : <OthersMessage message={message} lastMessage={messages[lastMessageKey]} />
+                        }
+                    </div>
+                    <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
+                        read-receipts
+                    </div>
+                </div >
+            )
+        })
+
+    }
+
+    if (!chat) return 'loading...'
+
+    return (
+        <div className="chat-feed">
+            <div className="chat-title-container">
+                <div className="chat-title">{chat?.title}</div>
+                <div className="chat-subtitle">
+                    {chat.people.map((person) => ` ${person.person.username}`)}
+                </div>
+            </div>
+            {renderMessages()}
+            <div style={{ width: '100px' }} />
+            <div className="message-form-container">
+                <MessageForm {...props} chatId={activeChat} />
+            </div>
         </div>
     );
 }
