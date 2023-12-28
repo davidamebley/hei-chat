05c31df7f01fabec874b198b6a54299b34b36ff9
David Amebley <davameb@gmail.com>
2023-12-28 03:36:39 +0200

    Add input field and form handling to MessageForm component

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
+import { sendMessage, isTyping } from 'react-chat-engine';
 
-const MessageForm = () => {
+const MessageForm = (props) => {
     const [value, setValue] = useState('');
+    const { chatId, creds } = props;
 
-    const handleSubmit = () => { }
-    const handleChange = () => { }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const text = value.trim();
+
+        if (text.length > 0) sendMessage(creds, chatId, { text })
+    }
+    const handleChange = (event) => {
+        setValue(event.target.value);
+
+
+        isTyping(props, chatId)
+    }
 
     return (
         <form className="message-form" onSubmit={handleSubmit}>
