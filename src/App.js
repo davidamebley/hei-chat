4678d6b49428e4140aebb0a6f913270aff2ca6dd
David Amebley <davameb@gmail.com>
2023-12-29 05:22:01 +0200

    Add checks to the main App component to show the login form or chat screen depending on whether the user is logged in.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { ChatEngine } from "react-chat-engine";
 
 import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
 import "./App.css";
 
 const App = () => {
+    // If the user is not logged in, render the login form
+    if (!localStorage.getItem('userName')) return <LoginForm />
+    // Otherwise, render the chat feed
     return (
         <ChatEngine
             height="100vh"
